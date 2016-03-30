2d537f524eeb695f5f5c7a0dceb5f0c08ac66bcc
Juan Carlos Coral <juan.carlos@planet108.de>
2016-03-30 08:09:24 -0300

    Initial Commit

---
+++ senalizacion.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt">
+	<head>
+		<meta charset="utf-8" />
+		<title>ZONUM - Se&ntilde;alizaci&oacute;n</title>
+
+		<!--[if IE]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/	html5.js"></script><![endif]-->
+		<!--[if lte IE 7]>
+		<script src="js/IE8.js" type="text/javascript"></script><![endif]-->
+		<!--[if lt IE 7]>
+ 
+		<link rel="stylesheet" type="text/css" media="all" href="css/ie6.css"/><![endif]-->
+
+	</head>
+
+	<body>
+	</body>
+
+</html>
